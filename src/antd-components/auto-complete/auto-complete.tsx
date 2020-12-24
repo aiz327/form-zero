65a7739b826b19414bfcccd17fd89f63901cb31c
zjgk <935299060@qq.com>
2020-12-24 10:34:35 +0800

    feat 完善组件扩展参数

--- src/antd-components/auto-complete/auto-complete.tsx
+++ src/antd-components/auto-complete/auto-complete.tsx
@@ -7,19 +7,19 @@ import './auto-complete.css';
 const { Option } = AutoComplete;
 /**
    * @description 自动搜索
-   * @param {boolean} currentShow 是否展示当前输入选项
+   * @param {function} requestFunc 请求接口回调
    * @param {boolean} highlightStatus 是否高亮
    * @param {string} delayTime 防抖搜索毫秒时间
-   * @param {function} requestFunc 请求接口回调
+   * @param {boolean} currentShow 是否展示当前输入选项
    * @param {object} extendParams 附带请求的扩展参数
    */
 export default function Autocomplete(props: any) {
-  const { requestFunc, highlightStatus = false, delayTime = 0, currentShow = false } = props;
+  const { requestFunc, highlightStatus = false, delayTime = 0, currentShow = false,extendParams={} } = props;
   const [data, setData] = useState<any>([]);
   const [options] = useState<any>(props.options || []);
   const [keyword, setKeyword] = useState<string>('');
   useEffect(() => {
-    requestFunc && requestFunc({ value: keyword }).then((res: any) => {
+    requestFunc && requestFunc({ value: keyword,...extendParams}).then((res: any) => {
       console.log('fetch', res)
       let getData = [...res];
       if (currentShow && keyword) getData.unshift({ value: keyword })
@@ -27,7 +27,7 @@ export default function Autocomplete(props: any) {
     })
   }, [keyword && options.length === 0])
   const onSearchFuc = (value: string) => setKeyword(value)
-  const children = options.length && data.map((item: any, index: string) =>
+  const children = options.length===0 && data.map((item: any, index: string) =>
     <Option value={item.value} key={index} >
       {highlightStatus ? <span className="hight-warp" dangerouslySetInnerHTML={{ __html: highlight(item.value, keyword) }}></span> : item.value}
     </Option>);
